84135b49a0afbfb2f75d66ef9ea9be6c48b437d3
jruberg <rubergly@gmail.com>
2011-09-16 23:37:52 -0400

    partially done reformatting functions into script format

--- weswings-parse.rb
+++ weswings-parse.rb
@@ -6,72 +6,79 @@ require 'hpricot'
 
 WESWINGS_URL = 'http://www.weswings.com'
 
-def download_pdfs
-
-  doc = open(WESWINGS_URL) { |f| Hpricot(f) }
-  pdfs = (doc/"table#table2 td:first-child > p a").collect do |e|
-    e.attributes['href']
-  end.reject do |p|
-    p == ""
-  end.collect do |p|
-    p.sub '.pdf', ''
-  end
-
-  Net::HTTP.start("weswings.com") do |http|
-    pdfs.reject {|pdf| File.exists?("pdf/weswings-#{pdf}.pdf")}.each do |pdf|
-      puts "Downloading PDF - #{pdf}.pdf"
-      resp = http.get("/#{pdf}.pdf")
-      open("pdf/weswings-#{pdf}.pdf", "wb") do |file|
-        file.write resp.body
-      end
-    end
-  end
-
-  return pdfs.collect {|pdf| "weswings-#{pdf}"}
-end
-
-def pdf_to_txt(ns)
-  ns.each do |n|
-    if !File.exists? "txt/#{n}.txt"
-      # use docsplit to convert to text
-      puts "#{n}.txt --> #{n}.pdf"
-      Docsplit.extract_text(Dir["pdf/#{n}.pdf"], :output => 'txt/')
-
-      # read in text output
-      ls = []
-      File.open("txt/#{n}.txt", "r") do |f|
-        while l = f.gets do ls << l end
-      end
-
-      # potentially filter out some garbage lines
-      ls.reject! {|l| l.length <= 2}
-
-      puts "CLEANING UP"
-
-      # fix up some potentially mangled "Lunch Specials" and "Dinner Entrees"
-      ls.collect! do |l|
-        if str_just_contains(l, "Lunch Specials\n")
-          return "Lunch Specials"
-        elsif str_just_contains(l, "Dinner Entrees\n")
-          return "Dinner Entrees"
-        else
-          return l
-        end
-      end
-
-      # write back the cleaned up text
-      File.open("txt/#{n}'txt", "w") do |f|
-        while l = ls.shift do f.write l end
-      end
-    end
-  end
-end
-
-# check if str is composed entirely of characters in the str chars
+# Check if `str` is composed entirely of characters in the string `chars`
 def str_just_contains(str, chars)
   puts str.inspect, chars.inspect
   str.each_char.all? {|c| chars.include? c}
 end
+
+
+#### Download PDFs from WesWings
+
+# Load the WesWings page, get all links in the menu area, filter out empty
+# links and remove the .pdf extensions.
+doc = open WESWINGS_URL {|f| Hpricot f}
+links = (doc/"#table2 td:first-child > p a").collect {|e| e.attributes['href']}
+names = links.reject {|l| p == ""}.collect {|l| l.sub '.pdf', ''}
+
+# Download the PDFs
+Net::HTTP.start("weswings.com") do |http|
+  names.reject {|n| File.exists? "pdf/weswings-#{n}.pdf"}.each do |n|
+    puts "Downloading PDF - #{n}.pdf"
+    resp = http.get("/#{n}.pdf")
+    open("pdf/weswings-#{n}.pdf", "wb") do |file|
+      file.write resp.body
+    end
+  end
+end
+
+#### Dump the TXT versions of PDFs
+
+# Delete all previously generated .txt files.
+File.delete *Dir['txt/*']
+
+# Generate dirty .txt dumps for the PDFs downloaded
+names.each do |n|
+  puts "weswings-#{n}.pdf --> weswings-#{n}.txt"
+  Docsplit.extract_text Dir["pdf/weswings-#{n}.pdf"], :output => 'txt/'
+end
+
+# Clean up the dirty .txt dumps.
+names.each do |n|
+
+  # Read in the dirty file.
+  ls = []
+  File.open "txt/weswings-#{n}.txt", "r" do |f|
+    while l = f.gets
+      ls << l
+    end
+  end
+
+  # Remove short lines; sometimes the text is formatted strangely and creates
+  # single-character artifact lines.
+  ls.reject! {l| l.lenght <= 2}
+
+  # Clean up lines that look mostly like "Lunch Specials" or "Dinner Entrees".
+  # This is necessary in case the weird formatting moved some letters to
+  # their own lines.
+  ls.collect! do |l|
+    if str_just_contains l, "Lunch Specials\n"
+      return "Lunch Specials\n"
+    elsif str_just_contains l, "Dinner Entrees\n"
+      return "Dinner Entrees\n"
+    else
+      return l
+    end
+  end
+
+  # Write the cleaned-up version.
+  File.open "txt/weswings-clean-#{n}.txt", "w" do |f|
+    while l = ls.shift
+      f.write l
+    end
+  end
+end
+
 
 def gather_items_from_txt(n)
   begin
