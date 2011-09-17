70757b2cfd32d65c5737435ab1de95ff40f6cefd
jruberg <rubergly@gmail.com>
2011-09-16 23:45:25 -0400

    comment un-restructured section

--- weswings-parse.rb
+++ weswings-parse.rb
@@ -84,74 +84,106 @@ names.each do |n|
     end
   end
 end
+  if !File.exists? "txt/#{n}.txt"
+    # use docsplit to convert to text
+    puts "#{n}.txt --> #{n}.pdf"
+    Docsplit.extract_text(Dir["pdf/#{n}.pdf"], :output => 'txt/')
 
-
-def gather_items_from_txt(n)
-  begin
+    # read in text output
+    ls = []
     File.open("txt/#{n}.txt", "r") do |f|
-      ls = []
       while l = f.gets do ls << l end
-
-      items = []
-      in_dinner = false
-      entree = []
-
-      ls.reject do |l|
-        ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
-         'Sunday'].collect{|d| d + "\n"}.include? l
-      end.each do |l|
-        if l == "Dinner Entrees\n" || l == "Lunch Specials\n"
-          in_dinner = l == "Dinner Entrees\n"
-        elsif l == "\n"
-          puts entree
-          item = menu_item entree
-          item[:meal] = in_dinner ? :dinner : :lunch
-          items << item
-          entree = []
-        else
-          entree << l
-        end
-      end
-
-      return items
     end
-  rescue IOError => err
-    puts "Exception: #{err}"
-  end
-end
 
-def menu_item(lines)
-  desc = ([lines[0].squeeze('.').split(' - ')[1].split(' . ')[0]] +
-          lines[1..-1]).join(' ').gsub(/\n/, ' ').squeeze(' ').strip
-  return {
-    :name => lines[0].split(' - ')[0],
-    :price => lines[0].split('$')[1].to_f,
-    :desc => desc
-  }
-end
+    # potentially filter out some garbage lines
+    ls.reject! {|l| l.length <= 2}
 
-def parse_txt(ns)
-  ns.each do |n|
-    items = gather_items_from_txt n
-    File.open("parsed/#{n}.txt", "w") do |f|
-      puts "DUMPING PARSED TXT"
-      f.write("### Lunch")
-      items.select {|item| item[:meal] == :lunch}.each do |item|
-        f.write "#### #{item[:name]} (#{item[:price]})"
-        f.write item[:desc]
+    puts "CLEANING UP"
+
+    # fix up some potentially mangled "Lunch Specials" and "Dinner Entrees"
+    ls.collect! do |l|
+      if str_just_contains(l, "Lunch Specials\n")
+        return "Lunch Specials"
+      elsif str_just_contains(l, "Dinner Entrees\n")
+        return "Dinner Entrees"
+      else
+        return l
       end
-      f.write("### Dinner")
-      items.select {|item| item[:meal] == :dinner}.each do |item|
-        f.write "#### #{item[:name]} (#{item[:price]})"
-        f.write item[:desc]
-      end
+    end
+
+    # write back the cleaned up text
+    File.open("txt/#{n}'txt", "w") do |f|
+      while l = ls.shift do f.write l end
     end
   end
 end
 
-# FIXME - pull PDF, set PDF filename
-# name = 'weswings-9-12-11'
-names = download_pdfs
-puts names
-pdf_to_txt names
-parse_txt names
+# def gather_items_from_txt(n)
+#   begin
+#     File.open("txt/#{n}.txt", "r") do |f|
+#       ls = []
+#       while l = f.gets do ls << l end
+
+#       items = []
+#       in_dinner = false
+#       entree = []
+
+#       ls.reject do |l|
+#         ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+#          'Sunday'].collect{|d| d + "\n"}.include? l
+#       end.each do |l|
+#         if l == "Dinner Entrees\n" || l == "Lunch Specials\n"
+#           in_dinner = l == "Dinner Entrees\n"
+#         elsif l == "\n"
+#           puts entree
+#           item = menu_item entree
+#           item[:meal] = in_dinner ? :dinner : :lunch
+#           items << item
+#           entree = []
+#         else
+#           entree << l
+#         end
+#       end
+
+#       return items
+#     end
+#   rescue IOError => err
+#     puts "Exception: #{err}"
+#   end
+# end
+
+# def menu_item(lines)
+#   desc = ([lines[0].squeeze('.').split(' - ')[1].split(' . ')[0]] +
+#           lines[1..-1]).join(' ').gsub(/\n/, ' ').squeeze(' ').strip
+#   return {
+#     :name => lines[0].split(' - ')[0],
+#     :price => lines[0].split('$')[1].to_f,
+#     :desc => desc
+#   }
+# end
+
+# def parse_txt(ns)
+#   ns.each do |n|
+#     items = gather_items_from_txt n
+#     File.open("parsed/#{n}.txt", "w") do |f|
+#       puts "DUMPING PARSED TXT"
+#       f.write("### Lunch")
+#       items.select {|item| item[:meal] == :lunch}.each do |item|
+#         f.write "#### #{item[:name]} (#{item[:price]})"
+#         f.write item[:desc]
+#       end
+#       f.write("### Dinner")
+#       items.select {|item| item[:meal] == :dinner}.each do |item|
+#         f.write "#### #{item[:name]} (#{item[:price]})"
+#         f.write item[:desc]
+#       end
+#     end
+#   end
+# end
+
+# # FIXME - pull PDF, set PDF filename
+# # name = 'weswings-9-12-11'
+# names = download_pdfs
+# puts names
+# pdf_to_txt names
+# parse_txt names
