2311b94659b1ca10a8124069a43e1e02203e2ffc
jruberg <rubergly@gmail.com>
2011-09-20 15:57:24 -0400

    succesfully parse this week's s&c menu

--- starandcrescent-parse.rb
+++ starandcrescent-parse.rb
@@ -87,7 +87,7 @@ Dir["#{DIGEST_DIR}/*.txt"].each do |d|
     if date_line(l)
       files[current_day] = day_ls unless day_ls.empty?
       current_day = date_s(l)
-      day_ls.clear
+      day_ls = []
     else
       day_ls << l
     end
@@ -102,7 +102,76 @@ Dir["#{DIGEST_DIR}/*.txt"].each do |d|
     end
     puts "--> #{txt_loc k}"
   end
+end
 
 
+#### Make a blog-formatted post for each day
 
+Dir["#{TXT_DIR}/*.txt"].each do |d|
+
+  n = d.split('/')[-1].split('.txt')[0]
+
+  ls = []
+  File.open d, 'r' do |f|
+    while l = f.gets
+      ls << l
+    end
+  end
+
+  # Collect the lunch and dinner items separately.
+  lunch_ls = []
+  dinner_ls = []
+  in_dinner = false
+  ls.each do |l|
+    in_dinner ||= l.index('Dinner') == 0
+    array = in_dinner ? dinner_ls : lunch_ls
+    avoid = in_dinner ? "Dinner" : "Lunch"
+    # Avoid the 'Dinner - bla bla' and 'Lunch - bla bla'
+    if new_menu_item l
+      array << l.split(avoid + ' ')[-1].split(avoid)[-1][2..-1]
+    else
+      array << l
+    end
+  end
+
+  # Split apart the menu items into their components
+  lunch = {}
+  dinner = {}
+  if !lunch_ls.empty?
+    lunch = {
+      :main => lunch_ls.reject {|l| l.index('Dessert') == 0 || l == "\n"}.collect {|l| l.strip}.join(''),
+      :dessert => lunch_ls.select {|l| l.index('Dessert') == 0}[0].split('Dessert ')[-1].split('Dessert')[-1].strip[2..-1]
+    }
+  end
+  if !dinner_ls.empty?
+    dinner = {
+      :dessert => dinner_ls.select {|l| l.index('Dessert') == 0}[0].split('Dessert')[-1].split('Dessert')[-1].strip[2..-1]
+    }
+    # this is a lazy heuristic at best; it assumes that when there's no comma
+    # separating the salad there are no commas at all. hopefully that'll
+    # always be the case.
+    if dinner_ls[0].include? ','
+      dinner[:salad] = dinner_ls[0].split(',')[0]
+      dinner[:main] = (dinner_ls[0].split(',')[1..-1].join(',').strip + ' ' +
+        dinner_ls[1..-1].reject {|l| l.index('Dessert') == 0 || l == "\n"}
+        .collect {|l| l.strip}.join(' ')).squeeze(' ')
+    elsif dinner_ls[0].downcase.include? 'dressing'
+      dinner[:salad] = dinner_ls[0].split('Dressing')[0].split('dressing')[0] +
+        'Dressing'
+      dinner[:main] = dinner_ls[0].split('Dressing')[-1].split('dressing')[-1].strip +
+        dinner_ls[1..-1].reject {|l| l.index('Dessert') == 0 || l == "\n"}
+        .collect {|l| l.strip}.join(' ').squeeze(' ')
+    else
+      # just give up and don't provide a salad; if we're this messed up, then
+      # the salad will probably just show up in the entree name, which isn't
+      # the tend of the world.
+      dinner[:salad] = ""
+      dinner[:main] = dinner_ls.reject {|l| l.index('Dessert') == 0 || l ==
+        "\n"}.collect {|l| l.strip}.join(' ').squeeze(' ')
+    end
+
+  end
+
+
+  puts 'lunch', lunch_ls.inspect, lunch, 'dinner', dinner_ls.inspect, dinner
 end
