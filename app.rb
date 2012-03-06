5958195b6f113ea1eb35db793fb70a3b33ab9459
jruberg <rubergly@gmail.com>
2012-03-06 15:55:23 -0500

    update gemfile and put db view in app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'redis'
 
-get '/' do
-	"Hello, world"
+configure do
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+get '/db' do
+  lines = []
+
+  REDIS.keys("*").each do |key|
+    case REDIS.type(key)
+    when "string"
+      lines << "#{key}: #{REDIS.get(key)}"
+    when "list"
+      lines << "#{key}: #{REDIS.lrange(key, 0, -1)}"
+    end
+  end
+  
+  lines.join "</br>"
 end
